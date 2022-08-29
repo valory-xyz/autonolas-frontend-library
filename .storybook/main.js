669538a7f3c2fb141763f56f1cd709569ceaf054
mohandast52 <mohandast52@gmail.com>
2022-08-29 16:18:22 +0530

    fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: ['@storybook/addon-essentials', '@storybook/addon-links', '@storybook/addon-storysource', '@storybook/addon-a11y', "@storybook/preset-ant-design"],
@@ -9,10 +11,33 @@ module.exports = {
         presets: [['react-app', { flow: false, typescript: true }]],
       },
     });
+
+    config.module.rules = config.module.rules.filter(
+      f => f.test.toString() !== '/\\.less$/'
+    );
+
+    config.module.rules.push({
+      test: /\.less$/,
+      use: ['style-loader', 'css-loader', {
+        loader: 'less-loader', options: {
+          lessOptions: {
+            modifyVars: {
+              'primary-color': 'green',
+              'font-size-base': '18px',
+              'font-family': '"manrope__regular", sans-serif',
+            },
+            javascriptEnabled: true,
+          }
+        }
+      }],
+      include: path.resolve(__dirname, '../'),
+    });
+
     config.resolve.extensions.push('.ts', '.tsx');
     config.node = Object.assign({}, config.node, {
       fs: 'empty',
     });
+
     return config;
   },
 };
