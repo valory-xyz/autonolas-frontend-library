6c13c5b8beef0f9773a7cfd50de2c88e1d374a72
mohandast52 <mohandast52@gmail.com>
2022-11-07 18:49:27 +0530

    Header component

---
+++ src/components/ecosystem/Header/Header.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from './Header';
+
+export default { title: 'Header' };
+
+export const Default = (): JSX.Element => (
+  <>
+    <Header title="Hello world!" />
+  </>
+);
