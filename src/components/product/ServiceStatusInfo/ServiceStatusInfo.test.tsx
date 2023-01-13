0a11a337ea0e9d933ed95ec5960d0b17e0e62ae8
mohandast52 <mohandast52@gmail.com>
2023-01-13 18:49:18 +0530

    add version

--- src/components/product/ServiceStatusInfo/ServiceStatusInfo.test.tsx
+++ src/components/product/ServiceStatusInfo/ServiceStatusInfo.test.tsx
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { ServiceStatusInfo } from './ServiceStatusInfo';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ServiceStatusInfo } from './ServiceStatusInfo';
 
-// describe('<Footer>', () => {
-//   it('should render footer', async () => {
-//     expect.hasAssertions();
-//     const { container } = render(<ServiceStatusInfo />);
-//     expect(container).toHaveTextContent('Valory');
-//   });
-// });
+describe('<ServiceStatusInfo>', () => {
+  it('should render empty content', async () => {
+    expect.hasAssertions();
+    const { container } = render(<ServiceStatusInfo />);
+    expect(container).toBeInTheDocument();
+  });
+});
