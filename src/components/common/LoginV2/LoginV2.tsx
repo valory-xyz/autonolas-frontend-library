bb229b050326de4d28ed1f15c3d0c659cf3753f8
mohandast52 <mohandast52@gmail.com>
2023-05-25 23:34:24 +0530

    more helpers

--- src/components/common/LoginV2/LoginV2.tsx
+++ src/components/common/LoginV2/LoginV2.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { ButtonProps } from 'antd';
 import {
   EthereumClient,
   w3mConnectors,
   w3mProvider,
 } from '@web3modal/ethereum';
 import { Web3Modal } from '@web3modal/react';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  configureChains,
+  createConfig,
+  WagmiConfig,
+  useAccount,
+  useBalance,
+  useNetwork
+} from 'wagmi';
 import { mainnet, gnosis, polygon, goerli, polygonMumbai } from 'wagmi/chains';
 import { Web3Button } from '@web3modal/react';
+import { GenericObject } from '../../../types';
 
 const chains = [mainnet, gnosis, polygon, goerli, polygonMumbai];
 const projectId = process.env.WALLET_PROJECT_ID as string;
@@ -23,8 +32,54 @@ const wagmiConfig = createConfig({
 });
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
-export const LoginV2 = () => {
-  console.log(process.env);
+type ConnectType = {
+  address?: string | null;
+  balance?: string | null;
+  chainId?: number | null;
+};
+
+type LoginProps = {
+  onConnect?: ({}: ConnectType) => void;
+  // onDisconnect?: () => void;
+  // onError?: (error: Error) => void;
+  // rpc?: GenericObject;
+  // buttonProps?: ButtonProps;
+  // /**
+  //  * TODO: make it more generic
+  //  * if the application uses both blockchain node & backend,
+  //  * then it is not considered as complete dapp. Hence point
+  //  * to mainnet only on production
+  //  */
+  // isDapp?: boolean;
+  // backendUrl?: string;
+  // supportedNetworks?: number[];
+};
+
+const useLoginHelpers = (account: `0x${string}`) => {
+  const { data } = useBalance({
+    address: account,
+  });
+
+  return {
+    balance: data,
+  };
+};
+
+export const LoginV2 = ({}: LoginProps) => {
+  // console.log(process.env);
+
+  const account = useAccount({
+    onConnect: (account) => {
+      console.log(account);
+      // const { balance } = useLoginHelpers(account);
+      // console.log(balance);
+    },
+  });
+
+  const { chain, chains } = useNetwork();
+
+  console.log(chain, chains);
+
   return (
     <>
       <WagmiConfig config={wagmiConfig}>
