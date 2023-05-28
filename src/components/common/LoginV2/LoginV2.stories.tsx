2a93599958490a7ee230f25deae53e42e780822a
mohandast52 <mohandast52@gmail.com>
2023-05-29 03:56:52 +0530

    login logic added

--- src/components/common/LoginV2/LoginV2.stories.tsx
+++ src/components/common/LoginV2/LoginV2.stories.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useNetwork } from 'wagmi';
 import { LoginV2 } from './LoginV2';
 
 export default { title: 'LoginV2' };
 
-export const Default = (): JSX.Element => <LoginV2 />;
+export const Default = (): JSX.Element => {
+  const { chain } = useNetwork();
+  console.log(chain);
+  return (
+    <>
+      <LoginV2 />
+    </>
+  );
+};
+
+export const NotDapp = (): JSX.Element => (
+  <LoginV2
+    // onConnect={(e) => console.log(e)}
+    // onDisconnect={() => console.log('disconnect')}
+    // buttonProps={{ type: 'link' }}
+    isDapp={false}
+    backendUrl="https://contribution-service-backend.staging.autonolas.tech"
+  />
+);
--- src/components/common/LoginV2/LoginV2.tsx
+++ src/components/common/LoginV2/LoginV2.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ButtonProps } from 'antd';
 import {
   EthereumClient,
@@ -10,20 +10,35 @@ import {
   configureChains,
   createConfig,
   WagmiConfig,
+
+  // hooks
   useAccount,
+  useConnect,
   useBalance,
-  useNetwork
+  useNetwork,
+
+  // types
+  Address,
 } from 'wagmi';
 import { mainnet, gnosis, polygon, goerli, polygonMumbai } from 'wagmi/chains';
 import { Web3Button } from '@web3modal/react';
+import { Button, Popover } from 'antd';
+import { WarningOutlined, CaretDownOutlined } from '@ant-design/icons';
+import {
+  COLOR,
+  SUPPORTED_NETWORKS,
+  SUPPORTED_TEST_NETWORKS,
+} from '../../../utils';
 import { GenericObject } from '../../../types';
+import { LoginContainer } from './styles';
+
+/**
+ * configs and helpers
+ */
 
 const chains = [mainnet, gnosis, polygon, goerli, polygonMumbai];
 const projectId = process.env.WALLET_PROJECT_ID as string;
 
-function HomePage() {
-  return <Web3Button />;
-}
 const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -32,6 +47,10 @@ const wagmiConfig = createConfig({
 });
 const ethereumClient = new EthereumClient(wagmiConfig, chains);
 
+/**
+ * types
+ */
+
 type ConnectType = {
   address?: string | null;
   balance?: string | null;
@@ -39,9 +58,10 @@ type ConnectType = {
 };
 
 type LoginProps = {
-  onConnect?: ({}: ConnectType) => void;
-  // onDisconnect?: () => void;
-  // onError?: (error: Error) => void;
+  // similar props to v1
+  onConnect?: ({}: ConnectType) => void; // TODO: pass balance, chainId
+  onDisconnect?: () => void;
+  onError?: (error: Error) => void;
   // rpc?: GenericObject;
   // buttonProps?: ButtonProps;
   // /**
@@ -50,43 +70,141 @@ type LoginProps = {
   //  * then it is not considered as complete dapp. Hence point
   //  * to mainnet only on production
   //  */
-  // isDapp?: boolean;
-  // backendUrl?: string;
-  // supportedNetworks?: number[];
+  isDapp?: boolean;
+  backendUrl?: string;
+  supportedNetworks?: number[];
+
+  // more props for v2
+  theme: 'light' | 'dark';
 };
 
-const useLoginHelpers = (account: `0x${string}`) => {
-  const { data } = useBalance({
-    address: account,
-  });
+const useLoginHelpers = (account: Address) => {
+  const { data } = useBalance({ address: account });
+  const { chain, chains } = useNetwork();
+  // console.log(chain, chains);
 
   return {
     balance: data,
+    chain,
   };
 };
 
-export const LoginV2 = ({}: LoginProps) => {
+export const LoginV2 = ({
+  onConnect: onConnectCb,
+  onDisconnect: onDisconnectCb,
+  onError: onErrorCb, // TODO: fix this
+  isDapp = true,
+  backendUrl,
+  supportedNetworks,
+  theme = 'light',
+}: LoginProps) => {
   // console.log(process.env);
+  const [account, setAccount] = React.useState<Address | undefined>(undefined);
+  const { chain, chains } = useNetwork();
+  const chainId = chain?.id;
 
-  const account = useAccount({
-    onConnect: (account) => {
-      console.log(account);
-      // const { balance } = useLoginHelpers(account);
-      // console.log(balance);
+  useAccount({
+    onConnect: ({ address }) => {
+      setAccount(address);
+    },
+    onDisconnect: () => {
+      setAccount(undefined);
+      if (onDisconnectCb) onDisconnectCb();
     },
   });
 
-  const { chain, chains } = useNetwork();
+  // useConnect({
+  //   onError: (error) => {
+  //     if (onErrorCb) onErrorCb(error);
+  //   },
+  // });
 
-  console.log(chain, chains);
+  useEffect(() => {
+    if (account && onConnectCb) {
+      onConnectCb({ address: account });
+    }
+  }, [account, onConnectCb]);
+
+  const unsupportedText = (
+    <>
+      <WarningOutlined />
+      Unsupported network
+    </>
+  );
+
+  const isStaging = () => {
+    return (backendUrl || '').includes('staging');
+  };
+
+  const isValidNetwork = () => {
+    // staging and other preview env
+    if (isStaging()) {
+      return SUPPORTED_TEST_NETWORKS.some((e) => e.id === chainId);
+    }
+
+    // production env
+    return chainId === 1;
+  };
 
   return (
-    <>
+    <LoginContainer>
+      {chainId && (
+        <>
+          {isDapp ? (
+            <>
+              {!(supportedNetworks || SUPPORTED_NETWORKS).includes(chainId) && (
+                <div className="unsupported-network">{unsupportedText}</div>
+              )}
+            </>
+          ) : (
+            <>
+              {!isValidNetwork() && (
+                <div className="unsupported-network">
+                  {unsupportedText}
+                  <Popover
+                    getPopupContainer={(triggerNode) =>
+                      triggerNode.parentNode as HTMLElement
+                    }
+                    content={
+                      <div>
+                        {isStaging() ? (
+                          <>
+                            {SUPPORTED_TEST_NETWORKS.map((e) => (
+                              <div key={`supported-chain-${e.id}`}>
+                                {e.name}
+                              </div>
+                            ))}
+                          </>
+                        ) : (
+                          <div>Ethereum</div>
+                        )}
+                      </div>
+                    }
+                    title="Switch to a supported network:"
+                  >
+                    <CaretDownOutlined />
+                  </Popover>
+                </div>
+              )}
+            </>
+          )}
+        </>
+      )}
+
       <WagmiConfig config={wagmiConfig}>
-        <HomePage />
+        <Web3Button balance="show" avatar="hide" />
       </WagmiConfig>
 
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
-    </>
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        themeMode={theme}
+        themeVariables={{
+          '--w3m-button-border-radius': '5px',
+          '--w3m-accent-color': COLOR.PRIMARY,
+          '--w3m-background-color': COLOR.PRIMARY,
+        }}
+      />
+    </LoginContainer>
   );
 };
