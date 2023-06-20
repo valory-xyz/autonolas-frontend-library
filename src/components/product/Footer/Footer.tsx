a3e9931fc50a0577ad38fbd6d062df461f52908d
mohandast52 <mohandast52@gmail.com>
2023-06-20 13:19:47 +0530

    add markdown and common disclaimer

--- src/components/product/Footer/Footer.tsx
+++ src/components/product/Footer/Footer.tsx
@@ -4,16 +4,25 @@ import { FooterContainer } from './styles';
 type FooterProps = {
   leftContent?: JSX.Element;
   rightContent?: JSX.Element;
+  centerContent?: JSX.Element;
 };
 
-export const Footer = ({ leftContent, rightContent }: FooterProps) => (
+const defaultCenterContent = (
+  <>
+    ©&nbsp;Valory&nbsp;
+    {new Date().getFullYear()}
+  </>
+);
+
+export const Footer = ({
+  leftContent,
+  rightContent,
+  centerContent,
+}: FooterProps) => (
   <FooterContainer>
     <div className="footer-left-content">{leftContent}</div>
 
-    <div className="footer-center">
-      ©&nbsp;Valory&nbsp;
-      {new Date().getFullYear()}
-    </div>
+    <div className="footer-center">{centerContent || defaultCenterContent}</div>
 
     <div className="footer-right-content">{rightContent}</div>
   </FooterContainer>
