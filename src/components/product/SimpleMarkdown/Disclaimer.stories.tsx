9dbae1120d7b730fb4d178468fdc7052230cd785
mohandast52 <mohandast52@gmail.com>
2023-06-20 14:01:34 +0530

    add more html

--- src/components/product/SimpleMarkdown/Disclaimer.stories.tsx
+++ src/components/product/SimpleMarkdown/Disclaimer.stories.tsx
@@ -16,7 +16,21 @@ A cookie is a small file of letters and numbers that we may store on your browse
 ## What types of cookies do we use?
 
 * Strictly necessary cookies. These are cookies that are required for the operation of our Websites. They include, for example, cookies that enable you to log into secure areas of our Websites, use a shopping cart, or make use of e-commerce payment processing services.
+  * One
+  * Two
 * Analytical/performance cookies. They allow us to recognize and count the number of visitors and to see how visitors move around our Websites when they are using it. This may help us improve how our Websites work, for example, by ensuring that users find what they are looking for easily.
+
+<ul>
+  <li>1. First Item</li>
+  <li>2. Second Item
+    <ul>
+    <li>2.1 Second Item Sub Item 1 </li>
+    <li>2.2 Second Item Sub Item 2</li>
+    <li>2.3 Second Item Sub Item 3</li>
+    </ul>
+  </li>
+  <li>3 Third Item </li>
+</ul>
 
 ## Add HTML
 
